d33bfc6895e692a20426bd0ebe744c14123999a7
shironzi <157195223+shironzi@users.noreply.github.com>
2024-03-29 21:43:03 +0800

    updated

--- js/carousel.js
+++ js/carousel.js
@@ -1,56 +1,13 @@
-const slides = document.querySelectorAll('.event-item');
-let slideIndex = 0;
-let intervalId = null;
-
-document.addEventListener("DOMContentLoaded", initializeSlider);
-
-document.getElementById('next-button').addEventListener('click', () => {
-    nextSlide();
-});
-
-document.getElementById('prev-button').addEventListener('click', () => {
-    prevSlide();
-});
-
-function initializeSlider() {
-    if (slides.length > 0) {
-        slides[slideIndex].classList.add("active");
-        intervalId = setInterval(nextSlide, 5000);
-    }
-}
-
-function showSlide(index) {
-    if (index >= slides.length) {
-        slideIndex = 0;
-    } else if (index < 0) {
-        slideIndex = slides.length - 1;
-    }
-
-    slides.forEach(slide => {
-        slide.classList.remove("active");
-    });
-    slides[slideIndex].classList.add("active");
-}
-
-function prevSlide() {
-    clearInterval(intervalId);
-    slideIndex--;
-    showSlide(slideIndex);
-}
-
-function nextSlide() {
-    slideIndex++;
-    showSlide(slideIndex);
-}
-
-window.onload = initializeSlider;
-
 let activeButton = document.querySelector(".eventButton1");
 let activeSlide = document.getElementById("newsSlide1");
+
+
+
 
 document.querySelectorAll(".eventButton1, .eventButton2, .eventButton3").forEach(button => {
     button.addEventListener("click", (event) => {
         let buttonClicked = event.target;
+        
 
         if (buttonClicked !== activeButton) {
             if (activeButton) {
